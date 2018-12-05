4e51f6979481beb1eee90f1bce0bf000c8ede125
Jose Oberto <joseoberto2@gmail.com>
2018-12-05 16:48:57 -0500

    [FIX] lines

--- src/services/UpdateDb/index.js
+++ src/services/UpdateDb/index.js
@@ -1,6 +1,5 @@
 import sqlite3 from 'sqlite3'
 
-
 let updateDb = new sqlite3.Database(':memory:', (err) => {
   if (err) {
     return console.error(err.message);
@@ -14,6 +13,5 @@ updateDb.close((err) => {
   }
   console.log('Close the database connection.');
 });
-
 
 export default updateDb
